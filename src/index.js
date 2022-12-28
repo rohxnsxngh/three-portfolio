aa6bfec15cc5a8965c03f79b6a59360dc800674a
rohxnsxngh <98181667+rohxnsxngh@users.noreply.github.com>
2022-12-28 11:08:51 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,16 +18,6 @@ let container;
 let camera, scene, renderer, clock, composer, planeMesh;
 let controls, water, upperwater, sun;
 let pointLight, ambientLight;
-let materials, current_material;
-let resolution;
-let effectController;
-let effect,
-  effect_home,
-  effect_edu,
-  effect_about,
-  effect_exp,
-  effect_contact1,
-  effect_contact2;
 let time = 0;
 
 function init() {
@@ -50,113 +40,7 @@ function init() {
   const far = 1000;
   scene.fog = new THREE.Fog(color, near, far);
 
-  setupGui();
-
-  // MATERIALS
-
-  materials = generateMaterials();
-  current_material = "matte";
-
-  // MARCHING CUBES
-  resolution = 28;
-
-  effect = new MarchingCubes(
-    resolution,
-    materials[current_material],
-    true,
-    true,
-    100000
-  );
-  effect.position.set(-3800, 50, 3800);
-  effect.scale.set(100, 100, 100);
-  effect.enableUvs = false;
-  effect.enableColors = false;
-  scene.add(effect);
-
-  //HOME
-
-  effect_home = new MarchingCubes(
-    resolution,
-    materials[current_material],
-    true,
-    true,
-    100000
-  );
-  effect_home.position.set(-3200, 50, 3750);
-  effect_home.scale.set(100, 100, 100);
-  effect_home.enableUvs = false;
-  effect_home.enableColors = false;
-  scene.add(effect_home);
-
-  // ABOUT
-  effect_about = new MarchingCubes(
-    resolution,
-    materials[current_material],
-    true,
-    true,
-    100000
-  );
-  effect_about.position.set(-3500, 0, 2898);
-  effect_about.scale.set(200, 200, 200);
-  effect_about.enableUvs = false;
-  effect_about.enableColors = false;
-  scene.add(effect_about);
-
-  //EXP1
-  effect_exp = new MarchingCubes(
-    resolution,
-    materials[current_material],
-    true,
-    true,
-    100000
-  );
-  effect_exp.position.set(-2600, 0, 2400);
-  effect_exp.scale.set(150, 150, 150);
-  effect_exp.enableUvs = false;
-  effect_exp.enableColors = false;
-  scene.add(effect_exp);
-
-  //EDU
-  effect_edu = new MarchingCubes(
-    resolution,
-    materials[current_material],
-    true,
-    true,
-    100000
-  );
-  effect_edu.position.set(-2600, 0, 3200);
-  effect_edu.scale.set(150, 150, 150);
-  effect_edu.enableUvs = false;
-  effect_edu.enableColors = false;
-  scene.add(effect_edu);
-
-  //CONTACT1
-  effect_contact1 = new MarchingCubes(
-    resolution,
-    materials[current_material],
-    true,
-    true,
-    100000
-  );
-  effect_contact1.position.set(-1975, 0, 1920);
-  effect_contact1.scale.set(25, 25, 25);
-  effect_contact1.enableUvs = false;
-  effect_contact1.enableColors = false;
-  scene.add(effect_contact1);
-
-  //CONTACT2
-  effect_contact2 = new MarchingCubes(
-    resolution,
-    materials[current_material],
-    true,
-    true,
-    100000
-  );
-  effect_contact2.position.set(-1875, 0, 1975);
-  effect_contact2.scale.set(25, 25, 25);
-  effect_contact2.enableUvs = false;
-  effect_contact2.enableColors = false;
-  scene.add(effect_contact2);
+  
 
   // Renderer
   renderer = new THREE.WebGLRenderer({
@@ -320,83 +204,7 @@ function render() {
 
   // marching cubes
 
-  if (effectController.resolution !== resolution) {
-    resolution = effectController.resolution;
-    effect.init(Math.floor(resolution));
-  }
-
-  if (effectController.isolation !== effect.isolation) {
-    effect.isolation = effectController.isolation;
-  }
-
-  updateCubes(
-    effect,
-    time,
-    effectController.numBlobs,
-    effectController.floor,
-    effectController.wallx,
-    effectController.wallz
-  );
-  updateCubes(
-    effect_home,
-    time,
-    effectController.numBlobs,
-    effectController.floor,
-    effectController.wallx,
-    effectController.wallz
-  );
-  updateCubes(
-    effect_about,
-    time,
-    effectController.numBlobs,
-    effectController.floor,
-    effectController.wallx,
-    effectController.wallz
-  );
-  updateCubes(
-    effect_exp,
-    time,
-    effectController.numBlobs,
-    effectController.floor,
-    effectController.wallx,
-    effectController.wallz
-  );
-  updateCubes(
-    effect_exp,
-    time,
-    effectController.numBlobs,
-    effectController.floor,
-    effectController.wallx,
-    effectController.wallz
-  );
-  updateCubes(
-    effect_edu,
-    time,
-    effectController.numBlobs,
-    effectController.floor,
-    effectController.wallx,
-    effectController.wallz
-  );
-  updateCubes(
-    effect_contact1,
-    time,
-    effectController.numBlobs,
-    effectController.floor,
-    effectController.wallx,
-    effectController.wallz
-  );
-  updateCubes(
-    effect_contact2,
-    time,
-    effectController.numBlobs,
-    effectController.floor,
-    effectController.wallx,
-    effectController.wallz
-  );
-  controls.update(clock.getDelta());
-  renderer.render(scene, camera);
-}
-
+ 
 init();
 animate();
 
@@ -405,55 +213,5 @@ console.log("Active Drawcalls:", renderer.info.render.calls);
 console.log("Textures in Memory", renderer.info.memory.textures);
 console.log("Geometries in Memory", renderer.info.memory.geometries);
 
-function generateMaterials() {
-  const materials = {
-    matte: new THREE.MeshPhongMaterial({ specular: 0x111111, shininess: 1 }),
-    colors: new THREE.MeshPhongMaterial({
-      color: 0xffffff,
-      specular: 0xffffff,
-      shininess: 2,
-      vertexColors: true,
-    }),
-  };
-  return materials;
-}
-
-function setupGui() {
-  effectController = {
-    material: "matte",
-    speed: 1.0,
-    numBlobs: 12,
-    resolution: 35,
-    isolation: 80,
-    floor: false,
-    wallx: false,
-    wallz: false,
-  };
-}
-
 // this controls content of marching cubes voxel field
-function updateCubes(object, time, numblobs, floor, wallx, wallz) {
-  object.reset();
-
-  //filling the field
-  const subtract = 12;
-  const strength = 1.2 / ((Math.sqrt(numblobs) - 1) / 4 + 1);
-
-  for (let i = 0; i < numblobs; i++) {
-    const ballx =
-      Math.sin(i + 1.26 * time * (1.03 + 0.5 * Math.cos(0.21 * i))) * 0.27 +
-      0.5;
-    const bally =
-      Math.abs(Math.cos(i + 1.12 * time * Math.cos(1.22 + 0.1424 * i))) * 0.77; // dip into the floor
-    const ballz =
-      Math.cos(i + 1.32 * time * 0.1 * Math.sin(0.92 + 0.53 * i)) * 0.27 + 0.5;
-
-    object.addBall(ballx, bally, ballz, strength, subtract);
-  }
-
-  if (floor) object.addPlaneY(2, 12);
-  if (wallz) object.addPlaneZ(2, 12);
-  if (wallx) object.addPlaneX(2, 12);
-
-  object.update();
 }
